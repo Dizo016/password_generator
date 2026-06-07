8856237d7f71076f7845425a9fd176f57ae77c58
Dizo016 <diegossilva2008@icloud.com>
2026-06-07 10:57:11 -0300

    feat: Adicionadas funções de randomização de caracteres

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//Seleção de elementos
+ const generatePasswordButton = document.querySelector("#generate-password")
+ const generatedPasswordElement = document.querySelector("#generated-password")
+
+//funções
+//letras, numeros e simbolos
+
+const getLetterLowerCase = () => {
+    return (String.fromCharCode(Math.floor(Math.random() * 26) + 97))
+    
+}
+
+const getLetterUpperCase = () => {
+    return (String.fromCharCode(Math.floor(Math.random() * 26) + 65))
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString()
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=></,.!@#$%&*-+"
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(getNumber());
+
+
+//eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log("Teste")
+})
